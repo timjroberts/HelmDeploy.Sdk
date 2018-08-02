d6f61f63d8339d074745a9adf8f384ad790c1dc6
timjroberts-inty <tim.roberts@inty.com>
2018-08-02 12:12:45 +0100

    allow service and ingress configuration to be independent

--- src/HelmDeploy.Sdk.Tasks/GenerateHelmApplicationFiles.cs
+++ src/HelmDeploy.Sdk.Tasks/GenerateHelmApplicationFiles.cs
@@ -40,35 +40,44 @@ namespace HelmDeploy.Sdk.Tasks
 
         public override bool Execute()
         {
-            CreateHelmIgnoreFile(new DirectoryInfo(TargetDirectory));
-            CreateHelmChartFile(new FileInfo(Path.Combine(TargetDirectory, "Chart.yaml")), ApplicationName, ApplicationDescription);
-            CreateYamlFileFromObject(
-                new FileInfo(Path.Combine(TargetDirectory, "values.yaml")),
-                new
-                {
-                    global = new
+            try
+            {
+                CreateHelmIgnoreFile(new DirectoryInfo(TargetDirectory));
+                CreateHelmChartFile(new FileInfo(Path.Combine(TargetDirectory, "Chart.yaml")), ApplicationName, ApplicationDescription);
+                CreateYamlFileFromObject(
+                    new FileInfo(Path.Combine(TargetDirectory, "values.yaml")),
+                    new
                     {
-                        application = new
+                        global = new
                         {
-                            name = ApplicationName,
-                            version = ApplicationVersion
-                        },
-                        repositoryPrefix = !string.IsNullOrEmpty(RepositoryPrefix) ? $"{RepositoryPrefix}/" : null
+                            application = new
+                            {
+                                name = ApplicationName,
+                                version = ApplicationVersion
+                            },
+                            repositoryPrefix = !string.IsNullOrEmpty(RepositoryPrefix) ? $"{RepositoryPrefix}/" : null
+                        }
+                    }
+                );
+
+                if (ProjectReferences != null && ProjectReferences.Length > 0)
+                {
+                    Directory.CreateDirectory(Path.Combine(TargetDirectory, "charts"));
+
+                    foreach (var projectReference in ProjectReferences)
+                    {
+                        CreateServiceHelmChart(projectReference);
                     }
                 }
-            );
 
-            if (ProjectReferences != null && ProjectReferences.Length > 0)
-            {
-                Directory.CreateDirectory(Path.Combine(TargetDirectory, "charts"));
-
-                foreach (var projectReference in ProjectReferences)
-                {
-                    CreateServiceHelmChart(projectReference);
-                }
+                return true;
             }
+            catch (Exception err)
+            {
+                Log.LogErrorFromException(err);
 
-            return true;
+                return false;
+            }
         }
 
         private void CreateServiceHelmChart(ITaskItem projectReference)
@@ -83,17 +92,30 @@ namespace HelmDeploy.Sdk.Tasks
 
             var serviceTemplatesDirectory = serviceTargetDirectory.CreateSubdirectory("templates");
 
-            CreateHelmChartFile(new FileInfo(Path.Combine(serviceTargetDirectory.FullName, "Chart.yaml")), serviceName, serviceDescription);
+            var configFile = new FileInfo(Path.Combine(serviceConfigDirectory.FullName, "config.yaml"));
 
-            var config = LoadYamlFile<IDictionary<string, object>>(new FileInfo(Path.Combine(serviceConfigDirectory.FullName, "config.yaml")));
+            var config = LoadYamlFile<IDictionary<string, object>>(configFile);
 
-            var ingressConfig = config.ContainsKey("ingress")
-                ? (Dictionary<object, object>)config["ingress"]
+            if (config == null) throw new ApplicationException($"Could not find a 'config.yaml' file for service '{serviceName}' ({serviceProjectDirectory})'.");
+
+            var portsConfig = config.ContainsKey("ports")
+                ? (List<object>)config["ports"]
+                : Enumerable.Empty<object>().ToList();
+
+            var serviceConfig = config.ContainsKey("service")
+                ? (Dictionary<object, object>)config["service"]
                 : null;
 
-            var portsConfig = ingressConfig != null && ingressConfig.ContainsKey("ports")
-                ? (List<object>)ingressConfig["ports"]
+            if (serviceConfig != null && portsConfig.Count == 0)
+            {
+                throw new ApplicationException($"Invalid configuration for service '{serviceName}' ({configFile.FullName}). 'service' section requires 'ports' section.");
+            }
+
+            var servicePortsConfig = serviceConfig != null && serviceConfig.ContainsKey("ports")
+                ? (List<object>)serviceConfig["ports"]
                 : Enumerable.Empty<object>().ToList();
+
+            CreateHelmChartFile(new FileInfo(Path.Combine(serviceTargetDirectory.FullName, "Chart.yaml")), serviceName, serviceDescription);
 
             CreateYamlFileFromObject(
                 new FileInfo(Path.Combine(serviceTemplatesDirectory.FullName, "deployment.yaml")),
@@ -142,13 +164,14 @@ namespace HelmDeploy.Sdk.Tasks
                                             portsConfig.Select(
                                                 (dynamic port) =>
                                                 {
+                                                    if (!port.ContainsKey("port")) throw new ApplicationException($"Invalid configuration for service '{serviceName}' ({configFile.FullName}). A port requires a 'port' value.");
+
                                                     var newPort = new Dictionary<string, object>();
+                                                    
+                                                    if (port.ContainsKey("name")) newPort["name"] = port["name"];
+                                                    if (port.ContainsKey("protocol")) newPort["protocol"] = port["protocol"];
 
-                                                    newPort["name"] = port["name"];
-                                                    newPort["protocol"] = port["protocol"];
                                                     newPort["containerPort"] = port["port"];
-
-                                                    if (port.ContainsKey("targetPort")) newPort["targetPort"] = port["targetPort"];
 
                                                     return newPort;
                                                 }
@@ -170,7 +193,7 @@ namespace HelmDeploy.Sdk.Tasks
                     kind = "Service",
                     metadata = new
                     {
-                        name = $"{serviceName}-service",
+                        name = $"{serviceName}",
                         labels = new
                         {
                             app = $"{ApplicationName}-{{{{ .Values.global.application.version }}}}"
@@ -178,16 +201,37 @@ namespace HelmDeploy.Sdk.Tasks
                     },
                     spec = new
                     {
-                        ports = portsConfig.Select(
+                        ports = servicePortsConfig.Select(
                             (dynamic port) =>
                             {
+                                if (!port.ContainsKey("port")) throw new ApplicationException($"Invalid configuration for service '{serviceName}' ({configFile.FullName}). A service port requires a 'port' value.");
+
                                 var newPort = new Dictionary<string, object>();
 
-                                newPort["name"] = port["name"];
-                                newPort["protocol"] = port["protocol"];
+                                if (port.ContainsKey("name")) newPort["name"] = port["name"];
+                                if (port.ContainsKey("protocol")) newPort["protocol"] = port["protocol"];
+
                                 newPort["port"] = port["port"];
 
-                                if (port.ContainsKey("targetPort")) newPort["targetPort"] = port["targetPort"];
+                                if (port.ContainsKey("targetPort"))
+                                {
+                                    var portNo = 0;
+
+                                    if (!int.TryParse(port["targetPort"], out portNo))
+                                    {
+                                        // targetPort is a string reference, check that the name exists in the configured ports
+                                        if (portsConfig.FirstOrDefault((dynamic p) => p.ContainsKey("name") && p["name"] == port["targetPort"]) == null)
+                                        {
+                                            throw new ApplicationException($"Invalid configuration for service '{serviceName}' ({configFile.FullName}). A service port with a 'targetPort' value of '{port["targetPort"]}' is referencing an unknown port.");
+                                        }
+                                    }
+
+                                    newPort["targetPort"] = port["targetPort"];
+                                }
+                                else
+                                {
+                                    newPort["targetPort"] = port["port"];
+                                }
 
                                 return newPort;
                             }
@@ -200,11 +244,33 @@ namespace HelmDeploy.Sdk.Tasks
                 }
             );
 
-            var httpPortsConfig = portsConfig.Where(
-                (dynamic port) => string.Equals(port["name"], "http", StringComparison.InvariantCultureIgnoreCase) || string.Equals(port["name"], "https", StringComparison.InvariantCultureIgnoreCase)
-            ).ToList();
+            var ingressPathsConfig = servicePortsConfig.Where((dynamic port) => port.ContainsKey("ingress"))
+                .Select((dynamic port) =>
+                    {
+                        var path = new Dictionary<string, object>()
+                        {
+                            { "path", port["ingress"].ContainsKey("path") ? port["ingress"]["path"] : "/" },
+                            { 
+                                "backend",
+                                new Dictionary<string, object>()
+                                { 
+                                    { "serviceName", $"{serviceName}" },
+                                    { "servicePort", port["port"] }
+                                }
+                            }
+                        };
 
-            if (httpPortsConfig.Count == 0) return;
+                        if (port["ingress"].ContainsKey("host"))
+                        {
+                            path.Add("host", port["ingress"]["host"]);
+                        }
+
+                        return path;
+                    }
+                )
+                .ToList();
+
+            if (ingressPathsConfig.Count == 0) return;
 
             CreateYamlFileFromObject(
                 new FileInfo(Path.Combine(serviceTemplatesDirectory.FullName, "ingress.yaml")),
@@ -226,40 +292,16 @@ namespace HelmDeploy.Sdk.Tasks
                     },
                     spec = new
                     {
-                        rules = httpPortsConfig.Select(
-                            port =>
+                        rules = new List<Dictionary<string, object>>()
+                        {
+                            new Dictionary<string, object>()
                             {
-                                var mapping = new Dictionary<string, object>()
                                 {
-                                    {
-                                        port["name"],
-                                        new { paths = new List<IDictionary<string, object>>()
-                                            {
-                                                new Dictionary<string, object>()
-                                                {
-                                                    { "path", port.ContainsKey("path") ? port["path"] : "/" },
-                                                    { 
-                                                        "backend",
-                                                        new Dictionary<string, object>()
-                                                        { 
-                                                            { "serviceName", $"{serviceName}-service" },
-                                                            { "servicePort", port["port"] }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                };
-
-                                if (port.ContainsKey("host"))
-                                {
-                                    mapping.Add("host", port["host"]);
+                                    "http",
+                                    new { paths = ingressPathsConfig }
                                 }
-
-                                return mapping;
                             }
-                        ).ToList()
+                        }
                     }
                 }
             );
